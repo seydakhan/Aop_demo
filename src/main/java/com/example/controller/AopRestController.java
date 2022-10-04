73bbffcce7f6610b00c0f436dd9d0e6f81c95db8
seydakhan <seydakarahan@hotmail.com>
2022-10-04 22:49:32 +0300

    first commit

--- src/main/java/com/example/controller/AopRestController.java
+++ src/main/java/com/example/controller/AopRestController.java
@@ -1,14 +1,17 @@
 package com.example.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/Aop")
 public class AopRestController {
 
-    @GetMapping("/Aop/{name}")
-    public String helloAop(@PathVariable String name){
-        return "Hello World! Hello" + name;
+    @RequestMapping("/getAop/{name}")
+    public ResponseEntity<String> helloAop(@PathVariable(name = "name", required = false) String name){
+        return ResponseEntity.ok("Hello World! Hello" + name);
     }
 }
